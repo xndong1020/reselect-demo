d7fd5a7e911eadce0fdb4c482e4e915e3798917d
isdance <isdance2004@hotmail.com>
2019-04-02 13:38:22 +1100

    init setup

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>About Page</p>
+  </div>
+)
+
+export default AboutPage
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Home</h1>
+    <p>Home Page</p>
+  </div>
+)
+
+export default HomePage
